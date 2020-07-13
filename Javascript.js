87a54f7a57f9f677225c9005b4f9997d9a562a15
Vladislav <vlad.almukov@mail.ru>
2020-07-13 19:36:30 +0300

    upd2

--- Javascript.js
+++ Javascript.js
@@ -1,11 +1,20 @@
+//var cell = document.getElementsByTagName("td");
+function changeColor (row)
+{ 
+	var r = row.getElementsByTagName("td");
+	console.log(r);
+	// row.style.backgroundColor = "red";
+		for (var i = 0; i < r.length; i++) {
+			r[i].style.backgroundColor = "red";
 
-alert('Я javascript!');
-let name = ("Джон");
-let admin = name;
-alert (admin);
-let Earth;
-Earth = ("Земля");
-let CurrentUser;
-CurrentUser = ("Текущий посетитель");
-alert (Earth);
+		}
+}
+ 
+	//  for (var i = 0; i < cell.length; i++) {
+	// 	cell[i].onclick = function (easy) {
+	// 		easy.target.style.background = "lime";
+	// 	}
+	// }
 
+ // window.onload = changeColor(r); 
+// смена цветов с заданного на цвет по умолчанию.
