36b6185fc41a3a18358c46c84e42a53ffdd75497
Vladislav <vlad.almukov@mail.ru>
2020-07-13 15:13:58 +0300

    js

---
+++ Javascript.js
@@ -0,0 +1,11 @@
+
+alert('Я javascript!');
+let name = ("Джон");
+let admin = name;
+alert (admin);
+let Earth;
+Earth = ("Земля");
+let CurrentUser;
+CurrentUser = ("Текущий посетитель");
+alert (Earth);
+
