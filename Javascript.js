1118fafe30be98fde8fe3a74d78361f409a28f30
Vladislav <vlad.almukov@mail.ru>
2020-07-22 18:57:13 +0300

    upd

--- Javascript.js
+++ Javascript.js
@@ -1,35 +1,23 @@
-//var cell = document.getElementsByTagName("td");
-function changeColor ()
-{ 
-	var r = document.getElementsByTagName("tr");
-	console.log(r);
-	for (var i = 0; i<r.length; i++) {
-		r[i].style.backgroundColor = "red";
-	
-		}	
-	if (r.style.backgroundColor == '#ff0000') {
-		return r.style.backgroundColor = '#0000ff';
-	}
-}
-function change ()
-{ 
-	var r = document.getElementById("4");
-	console.log(r);
-	r.style.background = "#BC8F8F";
-
- 	var r = document.getElementById("3");
-	console.log(r);
-	r.style.background = "#C0C0C0";
-
-	var r = document.getElementById("2");
-	console.log(r);
-	r.style.background = "#BC8F8F";
-
-	var r = document.getElementById("1");
-	console.log(r);
-	r.style.background = "#C0C0C0";
-	
-	}
-
-
-	
+console.log ('Request data')
+const p = new Promise (function(resolve,reject){
+    setTimeout(()=>{
+        console.log ('Preparing Data')
+        const process = function (status) {
+            status = "working";
+        }
+        resolve (process);
+    },2000)
+})
+    p.then(function(info){
+        return new Promise (function(resolve,reject){
+            setTimeout(()=>{
+                info.modified = true;
+               resolve(info);
+            },2000)
+        })
+})
+    .then(function (TotallyInfo){
+        console.log ("Data received", TotallyInfo);
+    })
+    .finally (()=>console.log('Finally'));
+    .catch (err => console.error('Error: ', err));
