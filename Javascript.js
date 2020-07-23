8b7e8917c4e866295fbd2aaef30b6bc6dec2df2f
Vladislav <vlad.almukov@mail.ru>
2020-07-23 17:28:42 +0300

    upd

--- Javascript.js
+++ Javascript.js
@@ -1,23 +1,43 @@
 console.log ('Request data')
 const p = new Promise (function(resolve,reject){
     setTimeout(()=>{
-        console.log ('Preparing Data')
-        const process = function (status) {
-            status = "working";
+        console.log('Preparing Data')
+        const PersonInfo = {
+            name: "Viktor" ,
+            gender: "male"
         }
-        resolve (process);
+         resolve (PersonInfo);
     },2000)
 })
     p.then(function(info){
         return new Promise (function(resolve,reject){
             setTimeout(()=>{
-                info.modified = true;
-               resolve(info);
+                haveJob = true;
+                resolve(info);
             },2000)
         })
 })
     .then(function (TotallyInfo){
         console.log ("Data received", TotallyInfo);
+        TotallyInfo.age = 101;
+        return TotallyInfo;
     })
+    .then(ModifiedData => {
+        console.log ("Modified", ModifiedData);
+    })
+    .catch(function(err){
+        console.log ("Error", err);
+    })
+
     .finally (()=>console.log('Finally'));
-    .catch (err => console.error('Error: ', err));
+
+    const sleep = ms => 
+    {return new Promise (resolve => {setTimeout(()=>
+        resolve(), ms)
+    })
+}
+sleep(5000).then (()=> console.log ('After 5 sec'));
+
+Promise.all([sleep(10000).then(()=> console.log ("..qweqwe")),])
+
+
